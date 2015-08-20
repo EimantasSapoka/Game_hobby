ddacd30b0b68f3095d129060a6d29eaf4db424c2
EimantasSapoka <eimantassapoka@gmail.com>
2015-08-20 17:01:04 +0100

    initial commit, added the current tutorial files

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count
+	{
+		public int maximum;
+		public int minimum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5,9);
+	public Count foodCount = new Count(1,5);
+	public GameObject exit;
+
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerWallTiles;
+
+	private Transform boardHolder;
+	private List<Vector3> gridPositions = new List<Vector3>();
+
+
+	void InitializeList(){
+		gridPositions.Clear ();
+
+		for (int x = 1; x < columns-1; x++) 
+		{
+			for (int y = 1; y < rows-1; y++) {
+				gridPositions.Add(new Vector3(x,y,0f));
+			}
+		}
+	}
+
+	void BoardSetup()
+	{
+		boardHolder = new GameObject ("Board").transform;
+		for (int x = -1; x < columns+1; x++) {
+			for (int y = -1; y < rows+1; y++) {
+				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+				if (x == -1 || x == columns || y == -1 || y == rows)
+					toInstantiate = outerWallTiles[Random.Range(0,outerWallTiles.Length)];
+				GameObject instance = Instantiate(toInstantiate, new Vector3(x,y,0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent(boardHolder);
+
+			}
+		}
+
+	}
+
+
+	Vector3 RandomPosition()
+	{
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
+	}
+
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max)
+	{
+		int randomObjectCount = Random.Range (min, max + 1);
+
+		for (int i = 0; i < randomObjectCount; i++) {
+
+			Vector3 randomPosition = RandomPosition ();
+			GameObject randomObject = tileArray[Random.Range(0, tileArray.Length)];
+			Instantiate(randomObject, randomPosition, Quaternion.identity);
+		}
+	}
+
+	public void SetupScene(int level)
+	{
+		BoardSetup ();
+		InitializeList ();
+		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Math.Log (level, 2);
+		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		Instantiate (exit, new Vector3 (columns-1, rows-1, 0f), Quaternion.identity);
+	}
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MovingObject {
+
+	public int playerDamage;
+
+	public AudioClip enemyAttackSound1;
+	public AudioClip enemyAttackSound2;
+
+	private Animator animator;
+	private Transform target;
+	private bool skipMove;
+
+	protected override void Start () {
+		GameManager.instance.AddEnemyToList (this);
+		animator = GetComponent<Animator> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		base.Start ();
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		if (skipMove)
+		{
+			skipMove = false;
+			return;
+		}
+		base.AttemptMove<T>(xDir,yDir);
+		skipMove = true;
+	}
+
+	public void MoveEnemy()
+	{
+		int xDir = 0;
+		int yDir = 0;
+
+		if (Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon)
+			yDir = target.position.y > transform.position.y ? 1 : -1;
+		else 
+			xDir = target.position.x > transform.position.x ? 1 : -1;
+		Debug.Log (string.Format("xDir: {0}, yDir: {1} ", xDir, yDir));
+		AttemptMove<Player> (xDir, yDir);
+
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		if (component is Player) {
+			Player hitPlayer = component as Player;
+			animator.SetTrigger ("Hit");
+			hitPlayer.LoseFood (playerDamage);
+			SoundManager.instance.RandomizeSfx(enemyAttackSound1, enemyAttackSound2);
+		}
+	}
+
+}

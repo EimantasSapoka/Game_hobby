e0830d4620f081882b023ee12caf477e0fd8b6b8
EimantasSapoka <eimantassapoka@gmail.com>
2015-08-26 12:36:50 +0100

    movement works

--- Hobby_Game/Assets/Scripts/Game/GameManager.cs
+++ Hobby_Game/Assets/Scripts/Game/GameManager.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public BoardManager boardManager;
 
-    public int level = 1;
+    public bool PlayerTurn;
 
-	// Use this for initialization
+    public int level = 1;
+    private float turnDelay = .1f;
+    public int playerFoodPoints = 100;
+    private bool enemiesMoving;
+    private List<Enemy> enemies;
+
+    // Use this for initialization
 	void Awake ()
 	{
 	    if (instance == null)
@@ -20,6 +27,8 @@ public class GameManager : MonoBehaviour
 	        DestroyObject(this.gameObject);
 	    }
 
+	    enemies = new List<Enemy>();
+
 	    DontDestroyOnLoad(this.gameObject);
 	    boardManager = GetComponent<BoardManager>();
 	    boardManager.GenerateLevel(level);
@@ -29,6 +38,51 @@ public class GameManager : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (PlayerTurn || enemiesMoving)
+        {
+            return;
+        }
+	    StartCoroutine(MoveEnemies());
 	}
+
+    protected IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0)
+        {
+            Debug.Log("Zero enemies");
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        PlayerTurn = true;
+        enemiesMoving = false;
+
+    }
+
+    public void GameOver()
+    {
+       // levelText.text = "After " + level + " days, you starved.";
+       // levelImage.SetActive(true);
+        enabled = false;
+
+        Invoke("LoadMenu", 3.0f);
+    }
+
+    private void LoadMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void AddEnemyToList(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
 }
